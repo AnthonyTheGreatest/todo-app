895ad434d8c2ef8aceb78a1604754b3c2e1a01f8
Miklós Milosevits <milosevitsmilos@gmail.com>
2023-10-27 16:09:56 +0200

    Alter commit.sh.

--- commit.sh
+++ commit.sh
@@ -1,26 +1,30 @@
-# Create new branch:
+#!/bin/bash
+
+# Create a new branch
 # git checkout -b new-branch
 
-# Make the script executable:
+# Make the script executable
 # chmod +x commit.sh
 
-# Check if a commit message argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <commit message>"
+# Prompt the user for a commit message
+read -p "Enter your commit message: " commit_message
+
+# Check if a commit message was provided
+if [ -z "$commit_message" ]; then
+    echo "Commit message is empty. Please provide a commit message."
     exit 1
 fi
 
-# Function for committing with a custom message
+# Function for committing with the custom message
 commit() {
-    # Add all changes, commit with the provided message, and push to the current branch
     git add .
-    git commit -m "$1"
+    git commit -m "$commit_message"
     git checkout main
-    git merge dev
-    git branch -d dev
+    git merge new-branch
+    git branch -d new-branch
     git push
     git checkout -b new-branch
 }
 
-# Call the commit function with the provided commit message
-commit "$1"
+# Call the commit function
+commit
