754b860f978ff8bf692019440dc458eeb53d716d
Miklós Milosevits <milosevitsmilos@gmail.com>
2023-10-31 15:31:01 +0100

    Add Cypress and change wireframe.

---
+++ src/ExampleApp.test.tsx
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import ExampleApp from './ExampleApp';
+
+describe('App', () => {
+  it.todo('renders a Subscribe button', () => {
+    // // Setup
+    // render(<App />);
+    // // Verify
+    // expect(screen.getByRole('button', { name: /subscribe/i })).toBeVisible();
+  });
+});
